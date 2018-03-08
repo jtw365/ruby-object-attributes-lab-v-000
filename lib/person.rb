6b0c49a041ce228ae7cda97609cc954ebda24d8a
Jonathan Williams <jtw365@live.missouristate.edu>
2018-03-08 18:09:30 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,3 +2,17 @@ class Person
   def name=(human_name)
     @name = human_name
   end
+
+  def name
+    @name
+  end
+
+  def job=(human_job)
+    @job = human_job
+  end
+
+  def job
+    @job
+  end
+
+end
